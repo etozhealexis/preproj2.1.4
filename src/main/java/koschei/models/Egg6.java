87902ff7259910a21e087bf86fe1a3f5ae7e8c1c
etozhealexis <alexisworksya@yandex.ru>
2023-03-25 20:11:40 +0300

    different types

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,12 +1,8 @@
 package koschei.models;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class Egg6 {
     private Needle7 needle;
 
-    @Autowired
     public Egg6(Needle7 needle) {
         this.needle = needle;
     }
